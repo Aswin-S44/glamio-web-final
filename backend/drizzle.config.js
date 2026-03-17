f5a1a4f6aa13a149f8157b777905cdfdea25ad4c
Aswin-S-T <aswinswebart15@gmail.com>
2026-03-17 21:17:10 +0530

    env variable naemchange

--- backend/drizzle.config.js
+++ backend/drizzle.config.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 const dbUrl =
   process.env.NODE_ENV === "development"
     ? process.env.DATABASE_URL_LOCAL
-    : process.env.FRONTEND_URL_PROD;
+    : process.env.DATABASE_URL_PROD;
 
 export default defineConfig({
   schema: "./src/db/schemas",
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -9,7 +9,7 @@ const { Pool } = pg;
 const dbUrl =
   process.env.NODE_ENV === "development"
     ? process.env.DATABASE_URL_LOCAL
-    : process.env.FRONTEND_URL_PROD;
+    : process.env.DATABASE_URL_PROD;
 
 const pool = new Pool({
   connectionString: dbUrl,
