f3bab93ac442ac16722140da0f6cc7aa80ded327
Aswin-S-T <aswinswebart15@gmail.com>
2026-05-12 19:51:51 +0530

    debuging

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -6,15 +6,26 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const dbUrl =
-//   process.env.NODE_ENV === "development"
-//     ? process.env.DATABASE_URL_LOCAL
-//     : process.env.DATABASE_URL_PROD;
-
-const dbUrl = process.env.DATABASE_URL_PROD;
+const dbUrl =
+  "postgresql://postgres:8979UOIDS23829382932932IKKKKKKKKKKKKK;;;IIII<<<kOOPO@db.ozvloioxtygihqyspslp.supabase.co:5432/postgres";
 
 const pool = new Pool({
   connectionString: dbUrl,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+pool
+  .connect()
+  .then((client) => {
+    console.log("✅ PostgreSQL connected successfully");
+
+    client.release();
+  })
+  .catch((err) => {
+    console.log("❌ PostgreSQL connection failed");
+    console.log(err);
+  });
 
 export const db = drizzle(pool);
